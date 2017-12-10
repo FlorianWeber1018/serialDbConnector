172c6ccd15f3dba5e4428647e62b43a6c40f10db
Florian Weber <florian.weber@live.de>
2017-12-10 21:41:48 +0100

    nomsg

--- ArduIoInterface.cpp
+++ ArduIoInterface.cpp
@@ -24,7 +24,7 @@ void ArduIoInterface::mainloop()
 {
   while(1){
     sendConfig();
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
   }
 }
 bool ArduIoInterface::sendConfig()
