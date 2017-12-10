6b4de2476bb851b2d0ed814a8d0d76669f9dda84
Florian Weber <florian.weber@live.de>
2017-12-10 19:02:06 +0100

    nomsg

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
- g++ -std=c++11 -pthread -lwiringPi `mysql_config --cflags` `mysql_config --libs`  main.cpp serialcmdinterface.cpp mysqlcon.cpp -o test
+ g++ -std=c++11 -pthread -lwiringPi `mysql_config --cflags` `mysql_config --libs`  main.cpp serialcmdinterface.cpp mysqlcon.cpp ArduIoInterface.cpp -o test
