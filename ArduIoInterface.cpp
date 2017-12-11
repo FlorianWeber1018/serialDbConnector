1c38c398fdcc6fbd15c46c38c7d6ce3238bded96
Florian Weber <florian.weber@live.de>
2017-12-11 23:51:12 +0100

    finished dir to ioDev

--- ArduIoInterface.cpp
+++ ArduIoInterface.cpp
@@ -25,10 +25,11 @@ ArduIoInterface::ArduIoInterface(std::string device, int baudrate, std::string h
 void ArduIoInterface::mainloop()
 {
   sendOutput(true);
-  sendOutput(false);
+  sendConfig(true);
 
   while(1){
     sendConfig(false);
+    sendOutput(false);
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
   }
 }
@@ -80,8 +81,8 @@ void ArduIoInterface::sendOutput(bool sendAll)
   }else{
     sqlQuery.append("\' AND IoValue.Outdated = TRUE;");
   }
-  std::cout << sqlQuery << endl;
-  return;
+  //std::cout << sqlQuery << endl;
+  //return;
 
   MYSQL_RES* result = sendCommand(sqlQuery);
   int colCnt = mysql_num_fields(result);
