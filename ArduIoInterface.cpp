9f69e20b9a6fffb2ce44ba26c6c37e4840707c20
Florian Weber <florian.weber@live.de>
2017-12-24 08:41:30 +0100

    nomsg

--- ArduIoInterface.cpp
+++ ArduIoInterface.cpp
@@ -17,11 +17,11 @@ void ArduIoInterface::serialDispatcher(std::string cmd)
   sqlQuery.append(" WHERE DeviceID = ");
   sqlQuery.append(device);
   sqlQuery.append(" AND PortType = ");
-  sqlQuery.append( cmd[2] );
+  sqlQuery.append( cmd.substr(2, 1) );
   sqlQuery.append(" AND Port = ");
-  sqlQuery.append( cmd[4] );
+  sqlQuery.append( cmd.substr(4, 1) );
   sqlQuery.append(" AND Pin = ");
-  sqlQuery.append( cmd[5] );
+  sqlQuery.append( cmd.substr(5, 1) );
   std::cout << "ArduIoInterface::sqlQuery=" << sqlQuery << std::endl;
 }
 bool ArduIoInterface::connect()
