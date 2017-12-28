9cc2720ad1659bb56aa273492ed3b12ccee6e385
Florian Weber <florian.weber@live.de>
2017-12-28 09:59:04 +0100

    nomsg

--- buildIoInterface.sh
+++ buildIoInterface.sh
@@ -1,1 +1,1 @@
- g++ -g -std=c++11 -pthread -lwiringPi `mysql_config --cflags` `mysql_config --libs`  mainIoInterface.cpp serialcmdinterface.cpp mysqlcon.cpp ArduIoInterface.cpp -o test
+ g++ -g -std=c++11 -pthread -lwiringPi `mysql_config --cflags` `mysql_config --libs`  mainIoInterface.cpp serialcmdinterface.cpp mysqlcon.cpp ArduIoInterface.cpp -o IoD
