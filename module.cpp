a0925a88ef3f0558ffa7f4f3f9aaf82009da3cbc
Florian Weber <florian.weber@live.de>
2017-12-28 10:14:19 +0100

    nomsg

---
+++ module.cpp
@@ -0,0 +1,9 @@
+#include "module__h"
+SignalSlots::connect(void* sender, const& Signal Signal, void* receiver, const& Slot slot)
+{
+
+}
+SignalSlots::connect(const& std::string DeviceID, const& std::string PortType, const& std::string Port, const& std::string Pin, void* signalRouter, void* receiver, const& Slot slot)
+{
+
+}
--- module.h
+++ module.h
@@ -3,31 +3,35 @@
 
 #include <vector>
 #include <string>
+namespace SignalSlots{
+  void connect(void* sender, const& Signal Signal, void* receiver, const& Slot slot);
+  void connect(std::string DeviceID, std::string PortType, std::string Port, std::string Pin, void* signalRouter, void* receiver, const& Slot slot);
+  struct Signal
+  {
+    int value;
+    int min;
+    int max;
+    int executionLevel;
+  };
+  struct Slot
+  {
+    int* value;
+    int min;
+    int max;
+    int executionLevel;
+  };
+  class module
+  {
+  public:
+    vector<Signal> signal;
+    vector<Slot> slot;
+    void connectToSignal(int slotNumber, Signal signal);
+    void connectToSignal(int slotNumber, std::string DeviceID, std::string PortType, std::string Port, std::string Pin);
+  private:
+  protected:
+  };
+}
 
-struct Signal
-{
-  int value;
-  int min;
-  int max;
-  int executionLevel;
-};
-struct Slot
-{
-  int* value;
-  int min;
-  int max;
-  int executionLevel;
-};
-class module
-{
-public:
-  vector<Signal> signal;
-  vector<Slot> slot;
-  void connectToSignal(int slotNumber, Signal signal);
-  void connectToSignal(int slotNumber, std::string DeviceID, std::string PortType, std::string Port, std::string Pin);
-private:
-protected:
-};
 
 
 
