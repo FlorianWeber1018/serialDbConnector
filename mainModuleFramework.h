bf3162f9318e7c15b5bb92171d4dd8934fc3c9af
Florian Weber <Florian.Weber@lawo.com>
2018-01-04 18:49:50 +0100

    nomsg

--- mainModuleFramework.h
+++ mainModuleFramework.h
@@ -1,10 +1,12 @@
 #ifndef mainModuleFramework__h
 #define mainModuleFramework__h
-#include "module.h"
-#include "signalRouter.h"
 
 
-
+/*
+void connect(SignalRouterIn* signalRouter, const& mySqlSignal _extSignal, Module* receiver, Slot* _Slot);       //done
+void connect(Module* sender, Signal* _Signal, Module* receiver, Slot* _Slot);                                                   //done
+void connect(Module* sender, Signal* _Signal, SignalRouterOut* signalRouter, const& mySqlSignal _extSignal);  //done
+*/
 int main();
 
 
