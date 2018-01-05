09cee0e537f8d1f4abe684964a8e97d0650e024a
Florian Weber <florian.weber@live.de>
2018-01-05 21:47:35 +0100

    nomsg

--- mainModuleFramework.cpp
+++ mainModuleFramework.cpp
@@ -1,28 +1,4 @@
 #include "mainModuleFramework.h"
-/*
-void connect(SignalRouterIn* signalRouter, const& mySqlSignal _extSignal, Module* receiver, Slot* _Slot)
-{
-  connect( signalRouter->createSignalIfNotexist(_extSignal) , receiver, _Slot );
-}
-
-void connect(Module* sender, Signal* _Signal, Module* receiver, Slot* _Slot)
-{
-  _Slot->value = &(_Signal->value);
-  _Signal->slots.append(_Slot);
-  sender->m_postModules.append(receiver);
-}
-
-void connect(Module* sender, Signal* _Signal, SignalRouterOut* signalRouter, const& mySqlSignal _extSignal)
-{
-  connect( sender, _Signal, signalRouter, signalRouter->createSlotIfNotExist(_extSignal) );
-}
-
-*/
-/*
-void connect(SignalRouterIn* signalRouter, const& mySqlSignal _extSignal, Module* receiver, Slot* _Slot);       //done
-void connect(Module* sender, Signal* _Signal, Module* receiver, Slot* _Slot);                                                   //done
-void connect(Module* sender, Signal* _Signal, SignalRouterOut* signalRouter, const& mySqlSignal _extSignal);  //done
-*/
 
 
 int main()
