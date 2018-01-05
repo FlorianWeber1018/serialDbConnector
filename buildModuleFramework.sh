ef420f9433b2575c3cf995c27433989efb193645
Florian Weber <florian.weber@live.de>
2018-01-05 22:34:21 +0100

    nomsg

--- buildModuleFramework.sh
+++ buildModuleFramework.sh
@@ -1,1 +1,1 @@
- g++ -g -std=c++11  `mysql_config --cflags` `mysql_config --libs` mainModuleFramework.cpp signalRouter.cpp module.cpp  module.cpp -o ModuleD
+ g++ -g -std=c++11  `mysql_config --cflags` `mysql_config --libs` mainModuleFramework.cpp signalRouter.cpp module.cpp  util.cpp -o ModuleD
