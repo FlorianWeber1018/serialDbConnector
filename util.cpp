278a81f218854e63a410cea0191a69b67f23c445
Florian Weber <florian.weber@live.de>
2018-01-05 22:39:21 +0100

    nomsg

--- util.cpp
+++ util.cpp
@@ -8,8 +8,8 @@ void connect(SignalRouterIn* signalRouter, mySqlSignal const& _extSignal, Module
 void connect(Module* sender, Signal* _Signal, Module* receiver, Slot* _Slot)
 {
   _Slot->value = &(_Signal->value);
-  _Signal->slots.append(_Slot);
-  sender->m_postModules.append(receiver);
+  _Signal->slots.push_back(_Slot);
+  sender->m_postModules.push_back(receiver);
 }
 
 void connect(Module* sender, Signal* _Signal, SignalRouterOut* signalRouter, mySqlSignal const& _extSignal)
