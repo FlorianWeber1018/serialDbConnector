9a515d6e84c0042dd51a9472e1abca76c0cf561d
Florian Weber <florian.weber@live.de>
2018-01-05 21:53:26 +0100

    nomsg

--- module.h
+++ module.h
@@ -44,10 +44,7 @@ protected:
   int getSignal(int slotNumber);
 };
 
-#include "signalRouter.h"
-void connect(SignalRouterIn* signalRouter, mySqlSignal const& _extSignal, Module* receiver, Slot* _Slot);       //done
-void connect(Module* sender, Signal* _Signal, Module* receiver, Slot* _Slot);                                                   //done
-void connect(Module* sender, Signal* _Signal, SignalRouterOut* signalRouter, mySqlSignal const& _extSignal);  //done
+
 
 
 #endif
---
+++ util.h
@@ -0,0 +1,11 @@
+#ifndef util__h
+#define util__h
+
+#include "signalRouter.h"
+#include "module.h"
+
+
+void connect(SignalRouterIn* signalRouter, mySqlSignal const& _extSignal, Module* receiver, Slot* _Slot);       //done
+void connect(Module* sender, Signal* _Signal, Module* receiver, Slot* _Slot);                                                   //done
+void connect(Module* sender, Signal* _Signal, SignalRouterOut* signalRouter, mySqlSignal const& _extSignal);  //done
+#endif
