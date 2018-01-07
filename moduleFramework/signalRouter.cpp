163d0a975d27dfc30ee1622ec56ef296f219b451
Florian Weber <florian.weber@live.de>
2018-01-07 12:28:30 +0100

    nomsg

--- moduleFramework/signalRouter.cpp
+++ moduleFramework/signalRouter.cpp
@@ -1,10 +1,11 @@
 #include "signalRouter.h"
 Signal* SignalRouterIn::createSignalIfNotexist(mySqlSignal const& extSignal)
 {
-  return signalMap[extSignal]&;
+
+  return signalMap[extSignal]*;
 }
 
 Slot* SignalRouterOut::createSlotIfNotExist(mySqlSignal const& extSignal)
 {
-  return slotMap[extSignal]&;
+  return slotMap[extSignal]*;
 }
