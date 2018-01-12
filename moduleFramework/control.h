870e93e22f30c0a39259af1e12711b4557a38f48
Florian Weber <florian.weber@live.de>
2018-01-12 20:26:32 +0100

    nomsg

--- moduleFramework/control.h
+++ moduleFramework/control.h
@@ -34,9 +34,9 @@ struct config_ServoPWM
   int input_max  = 30000;
   int input_min  = -30000;
   float incPWM_max = 255;
-  float incPWM_min = 30;
+  float incPWM_min = 40;
   float decPWM_max = 255;
-  float decPWM_min = 30;
+  float decPWM_min = 40;
 };
 
 class ServoPWM
