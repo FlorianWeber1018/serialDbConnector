90ed4ba586836bda5f034981460a71c05b229cb5
Florian Weber <florian.weber@live.de>
2018-01-12 22:20:58 +0100

    nomsg

--- moduleFramework/control.h
+++ moduleFramework/control.h
@@ -3,13 +3,13 @@
 #include <climits>
 struct config_PID
 {
-  float kp        = 3800;
+  float kp        = 2000;
     float up_max  = INT_MAX;
     float up_min  = INT_MIN;
   float ki        = 0;
     float ui_max  = INT_MAX;
     float ui_min  = INT_MIN;
-  float kd        = 500;
+  float kd        = 0;
     float ud_max  = INT_MAX;
     float ud_min  = INT_MIN;
 };
@@ -33,10 +33,10 @@ struct config_ServoPWM
 {
   int input_max  = 30000;
   int input_min  = -30000;
-  float incPWM_max = 255;
-  float incPWM_min = 50;
-  float decPWM_max = 255;
-  float decPWM_min = 50;
+  float incPWM_max = 150;
+  float incPWM_min = 30;
+  float decPWM_max = 150;
+  float decPWM_min = 30;
 };
 
 class ServoPWM
