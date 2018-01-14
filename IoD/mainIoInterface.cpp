5cb5813ec8131ae4f5ab560ec9643211f8b7af90
Florian Weber <florian.weber@live.de>
2018-01-14 22:20:22 +0100

    nomsg

--- IoD/mainIoInterface.cpp
+++ IoD/mainIoInterface.cpp
@@ -14,7 +14,7 @@ using namespace std;
 int main()
 {
 
-		std::this_thread::sleep_for(std::chrono::seconds(10));
+		//std::this_thread::sleep_for(std::chrono::seconds(10));
 		cout<<"app started"<<endl;
 		ArduIoInterface* m_ArduIoInterface = new ArduIoInterface("/dev/ttyACM0", 57600, "localhost", 3306, "IoD", "637013", "heizung");
 		while(1){
