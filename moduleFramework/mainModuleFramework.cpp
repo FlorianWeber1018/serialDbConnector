b15353885d7ca88a539c00cb00ca3ba70e54bafc
Florian Weber <florian.weber@live.de>
2018-01-16 20:09:09 +0100

    nomsg

--- moduleFramework/mainModuleFramework.cpp
+++ moduleFramework/mainModuleFramework.cpp
@@ -79,14 +79,14 @@ int main()
   Module_debug* modSec = new Module_debug();
   modSec->m_config.identifier = "second";
 
-  connectTimeSignal(    //CONNECT constant Signal -> requiredTemperature of PID
+  connectToTime(    //CONNECT constant Signal -> requiredTemperature of PID
     modRouterIn,
     "minute",
     modMin,
     modMin->getSlot("debugSlot")
   );
 
-  connectTimeSignal(    //CONNECT constant Signal -> requiredTemperature of PID
+  connectToTime(    //CONNECT constant Signal -> requiredTemperature of PID
     modRouterIn,
     "second",
     modSec,
