df9c52df82807590a27d8ddce41849775e015629
Florian Weber <florian.weber@live.de>
2018-02-10 14:25:59 +0100

    nomsg

--- moduleFramework/module.cpp
+++ moduleFramework/module.cpp
@@ -240,6 +240,7 @@ Module_3WayValve::Module_3WayValve()
 void Module_3WayValve::process()
 {
   int DC_inc, DC_dec;
+  std::cout << "PROCESS" << std::endl;
   if(getSignalValue("!EN")){
     DC_dec = m_config.pwmConfig->decPWM_max;
     DC_inc = 0;
