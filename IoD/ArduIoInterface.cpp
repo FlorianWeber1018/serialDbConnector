31935693f44277b2d88c33b00c0bb88f74f279d6
Florian Weber <florian.weber@live.de>
2018-05-26 23:00:54 +0200

    nomsg

--- IoD/ArduIoInterface.cpp
+++ IoD/ArduIoInterface.cpp
@@ -147,12 +147,12 @@ void ArduIoInterface::sendConfig(bool sendAll)
       }
     }
 
-    if(debuglevel&1){
+    //if(debuglevel&1){
       std::cout << "now flushing:";
       plotFlushStringToConsole(flushStr);
       std::cout << std::endl;
     }
-    serialFlush(flushStr);
+    //serialFlush(flushStr);
   }
   mysql_free_result(result);
 }
