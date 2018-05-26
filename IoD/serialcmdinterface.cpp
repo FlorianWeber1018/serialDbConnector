54913943110b727b8e94a1b15fa2b80cfcab82e0
Florian Weber <florian.weber@live.de>
2018-05-26 12:50:51 +0200

    nomsg

--- IoD/serialcmdinterface.cpp
+++ IoD/serialcmdinterface.cpp
@@ -146,7 +146,7 @@ void serialCmdInterface::Sending()
 			//if(rtr){
 				//rtr=false;
 			sendOne(temp);
-			std::cout << temp << std::endl;
+
 			std::this_thread::sleep_for(std::chrono::milliseconds(15));
 			bufOut.pop_front();
 			//}
