0136bdb133489bea506b55cb61fac909ed652423
Florian Weber <florian.weber@live.de>
2018-06-04 15:32:58 +0200

    nomsg

--- buildModuleFramework.sh
+++ buildModuleFramework.sh
@@ -1,4 +1,4 @@
 git pull
 cd moduleFramework
-g++ -g -std=c++17  `mysql_config --cflags` `mysql_config --libs` mainModuleFramework.cpp signalRouter.cpp paramRouter.cpp module.cpp modConMan.cpp util.cpp clock.cpp control.cpp ../IoD/mysqlcon.cpp -o ModuleD
+g++ -g -std=c++17  `mysql_config --cflags` `mysql_config --libs` mainModuleFramework.cpp signalRouter.cpp paramRouter.cpp module.cpp modConMan.cpp util.cpp clock.cpp control.cpp timer.cpp ../IoD/mysqlcon.cpp -o ModuleD
 cd ..
